997911e60d52282e10db60de47f0aa545c144b44
mary-del <60111442+mary-del@users.noreply.github.com>
2021-03-04 17:13:09 +0300

    Add files via upload

---
+++ bootstrap — копия/boot.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$("body").on('mousewheel', function(){
+		let e = $("#main_menu");
+		let offs = e.offset();
+		let y = offs.top;
+		if(y>=500){
+			$("#main_menu").css({
+				backgroundColor:'rgb(99, 99, 99)'
+			});
+			$(".container-1").css({
+				backgroundColor:'rgb(99, 99, 99)'
+			});
+		}else if(y<500){
+			$("#main_menu").css({
+				backgroundColor:'rgba(0,0,0,0)'
+			});
+			$(".container-1").css({
+				backgroundColor:'rgba(0, 0, 0, 0)'
+			});
+		}
+	});
+
+	$(".menue").click(function(){
+		$(".menue1").toggle(500);
+	});
+});
